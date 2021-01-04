bef8c8345dfa4ff94c57d34f331b4768f18661cc
toyukai <tsukawatari@gmail.com>
2021-01-04 16:42:22 +0900

    vie.js add

---
+++ js/bookmeter.js
@@ -0,0 +1,18 @@
+// 初期化処理
+(function() {
+
+
+  var app = new Vue({
+    el: '#app',
+    data: {
+      books : [
+        { title : '1', impressions : 'わ〜'},
+        { title : '2', impressions : 'わ〜い'},
+        { title : '3', impressions : 'わ〜いい'},
+        { title : '4', impressions : 'わ〜いいい'},
+      ]
+    }
+  })
+
+})();
+
